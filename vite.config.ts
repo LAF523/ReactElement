f79e1ee5d53f123d0d2f6f8593723b6cc7ddd863
aofei.li <aofei.li@pacteraedge.com>
2024-01-01 02:51:46 +0800

    feat: 集成vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -42,6 +43,14 @@ export default defineConfig(({ mode }) => {
             test: '#1CC0FF'
           }
         }
+      }
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom', //提供浏览器API以模拟浏览器环境
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html']
       }
     },
     server: {
