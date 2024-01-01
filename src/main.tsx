1e75b16bdba10b6b86ce73b96d3e49c325283d58
aofei.li <aofei.li@pacteraedge.com>
2024-01-01 21:18:53 +0800

    feat: 添加组件打包基础结构demo

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import 'normalize.css/normalize.css';
-import './common/common.less';
+import './styles/common.less';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
